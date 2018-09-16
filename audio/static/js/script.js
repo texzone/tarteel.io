fc52d9e9e4d6b8b7626a49d571c366bbfa6aa8bd
Abubakar Abid <a12d@stanford.edu>
2018-09-16 10:48:38 -0700

    changed end of ayah symbol

--- audio/static/js/script.js
+++ audio/static/js/script.js
@@ -55,7 +55,7 @@ function load_ayah_callback(data) {
   state = StateEnum.AYAH_LOADED;
   ayah_data = data;
   $("#mic").removeClass("recording");
-  $("#ayah-text").html(`<p class=ayah-quran-text>${data.line}۝</p>`)
+  $("#ayah-text").html(`<p class=ayah-quran-text>${data.line}◌</p>`)
   setLastAyah(data)
   $("#surah-num").text(data.surah);
   $("#ayah-num").text(data.ayah);
