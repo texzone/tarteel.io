07415198affaa892dfa909949e624ec213ab5d1d
Abubakar Abid <a12d@stanford.edu>
2018-11-20 15:07:50 -0800

    fixed data import

--- audio/views.py
+++ audio/views.py
@@ -14,7 +14,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from restapi.models import AnnotatedRecording, DemographicInformation
 from rest_framework.decorators import api_view
-from data import COUNTRIES
+from audio.data import COUNTRIES
 
 
 # =============================================== #
