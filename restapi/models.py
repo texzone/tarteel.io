8c90cc102c7fdf48e0e8e948d6368815b457e469
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2018-12-16 20:12:16 -0800

    [#139] Enables download of all audio recordings.
    - Adds an `is_evaluated` boolean field to the AnnotatedRecording model.

--- restapi/models.py
+++ restapi/models.py
@@ -12,12 +12,13 @@ class AnnotatedRecording(models.Model):
     recitation_mode = models.CharField(max_length=32, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)  # TODO(implement timeout)
     session_id = models.CharField(max_length=32)
+    is_evaluated = models.BooleanField(default=False)
 
 
 class DemographicInformation(models.Model):
     session_id = models.CharField(max_length=32, blank=True)
     # This could be used to store different platforms such as android,
-    # ios, web if different identificaiton methods are used for each one.
+    # ios, web if different identification methods are used for each one.
     platform = models.CharField(max_length=32, default='web')
     gender = models.CharField(max_length=32)
     qiraah = models.CharField(max_length=32, blank=True, null=True)
