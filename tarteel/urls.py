12ec616e5ec2fe9ea3b47a4a66a0ecdf9c6ced87
Anas Abou Allaban <piraka9011@hotmail.com>
2019-01-27 15:16:10 -0500

    Fix audio link url

--- tarteel/urls.py
+++ tarteel/urls.py
@@ -44,7 +44,6 @@ urlpatterns = [
     url(r'^download-full-dataset-csv/', audio.views.download_full_dataset_csv),
     # Audio App (Main Page)
     url(r'^$', audio.views.index, name='index'),
-    url(r'^audio/media/(?:(?P<filename>[-\w]+\.wav)/)?$', audio.views.audio_file),
     url(r'^privacy/', audio.views.privacy, name='privacy'),
     url(r'^mobile_app/', audio.views.mobile_app),
     url(r'^about/', audio.views.about),
