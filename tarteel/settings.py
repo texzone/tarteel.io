637b4c3932f51878db0dc6d9243ea575dc4170eb
Anas Abou Allaban <piraka9011@hotmail.com>
2019-02-21 09:03:03 -0500

    Added AWS env vars for DEBUG

--- tarteel/settings.py
+++ tarteel/settings.py
@@ -24,7 +24,17 @@ env = environ.Env(
     DEBUG=(bool, True)
 )
 env.read_env(str(ROOT.path('tarteel/.env')))
+DEBUG = env('DEBUG', bool, default=True)
 
+# Read AWS Environment Variables
+if 'SERVERTYPE' in os.environ and os.environ['SERVERTYPE'] == 'AWS Lambda':
+    import json
+    import os
+    json_data = open('zappa_settings.json')
+    env_vars = json.load(json_data)['dev']['environment_variables']
+    DEBUG = env_vars['DEBUG']
+    for key, val in env_vars.items():
+        os.environ[key] = val
 
 ALLOWED_HOSTS = ['www.tarteel.io', 'tarteel.io', '.tarteel.io', 'localhost', '127.0.0.1',
                  env('EC2_IP', str, default=''), env('EC2_IP1', str, default=''),
@@ -36,7 +46,7 @@ ALLOWED_HOSTS = ['www.tarteel.io', 'tarteel.io', '.tarteel.io', 'localhost', '12
 # ------------------------------------------------------------------------------
 SECRET_KEY = env('SECRET_KEY', str, default='development_security_key')
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env('DEBUG', bool, default=True)
+
 # Local time zone: http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 TIME_ZONE = env('TIME_ZONE', str, default='UTC')
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
